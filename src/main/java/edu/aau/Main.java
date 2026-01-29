ef5087c47b01f8245d3da275a12342bfbb588ea4
LidetuWendyifraw <lidetu190@gmail.com>
2026-01-29 09:18:02 +0300

    Initial project structure abd Maven setup

---
+++ src/main/java/edu/aau/Main.java
@@ -0,0 +1,23 @@
+package edu.aau;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+ @override
+
+    public void start(Stage stage) {
+        Label label = new Label("Airline Ticket System is Running!");
+        Scene scene = new Scene(new StackPane(label), 600, 400);
+
+        stage.setTitle("Airline Ticket System");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
