95965644aca7bb2d17c06bdd9107a69732894ec2
LidetuWendyifraw <lidetu190@gmail.com>
2026-01-30 17:17:00 +0300

    Update FlightRepository.java

--- src/main/java/edu/aau/FlightRepository.java
+++ src/main/java/edu/aau/FlightRepository.java
@@ -1,36 +1,48 @@
-package edu.aau;
-import java.io.*;
-import java.util.*;
+package edu.aau.repository;
+
+import edu.aau.model.Flight;
+import edu.aau.util.DatabaseUtil;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FlightRepository {
 
+    public void addFlight(Flight flight) throws Exception {
+        String sql = "INSERT INTO flights(flight_number, origin, destination, price, seats) VALUES(?,?,?,?,?)";
 
+        try (Connection conn = DatabaseUtil.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
 
-        private static final String FILE_NAME = "flights.txt";
-
-        public static List<Flight> getAllFlights() throws IOException {
-            List<Flight> flights = new ArrayList<>();
-            File file = new File(FILE_NAME);
-            if (!file.exists()) return flights;
-
-            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-                String line;
-                while ((line = br.readLine()) != null) {
-                    String[] data = line.split(":");
-                    flights.add(new Flight(
-                            data[0], data[1], data[2],
-                            Integer.parseInt(data[3]), data[4]
-                    ));
-                }
-            }
-            return flights;
+            ps.setString(1, flight.getFlightNumber());
+            ps.setString(2, flight.getOrigin());
+            ps.setString(3, flight.getDestination());
+            ps.setDouble(4, flight.getPrice());
+            ps.setInt(5, flight.getSeats());
+            ps.executeUpdate();
         }
+    }
 
-        public static void saveAllFlights(List<Flight> flights) throws IOException {
-            try (BufferedWriter bw = new BufferedWriter(new FileWriter(FILE_NAME))) {
-                for (Flight f : flights) {
-                    bw.write(f.toString());
-                    bw.newLine();
-                }
+    public List<Flight> getAllFlights() throws Exception {
+        List<Flight> flights = new ArrayList<>();
+        String sql = "SELECT * FROM flights";
+
+        try (Connection conn = DatabaseUtil.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                flights.add(new Flight(
+                        rs.getString("flight_number"),
+                        rs.getString("origin"),
+                        rs.getString("destination"),
+                        rs.getDouble("price"),
+                        rs.getInt("seats")
+                ));
             }
-}}
+        }
+        return flights;
+    }
+}
+
